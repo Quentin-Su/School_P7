ab76a59db119dda464c254c164735c74ee640ca6
Quentin-Su <Quentin.Sussetto@outlook.fr>
2023-03-30 23:38:57 +0200

    API - Auth Controller

---
+++ server/controllers/authController.js
@@ -0,0 +1,44 @@
+const { User } = require('../models/user'); 
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+
+exports.login = async (req, res) => {
+    if (!req.body.email || !req.body.password) return res.status(400).send({ message: 'Must have email and password' });
+
+    try {
+        const user = {
+            email: req.body.email,
+            password: crypto.createHash('sha256').update(req.body.password).digest('hex')
+        };
+            
+        const result = await User.findOne(user);
+
+        if (!result) return res.status(404).send({ message: 'User not found' });
+        else {
+            return res.status(200).json({
+                userId: result._id,
+                token: jwt.sign({ userId: result._id }, process.env.API_KEY, { expiresIn: '24h'})
+            });
+        }
+    }
+    catch (e) {
+        return res.status(500).send({ message: e.message });
+    }
+};
+
+exports.signup = async (req, res) => {
+    if (!req.body.email || !req.body.password) return res.status(400).send({ message: 'Must have email and password' });
+
+    try {
+        const user = new User({
+            email: req.body.email,
+            password: crypto.createHash('sha256').update(req.body.password).digest('hex')
+        });
+            
+        await User.create(user);
+		return res.status(201).json({ message: 'User Created' });
+    }
+    catch (e) {
+        return res.status(500).send({ message: e.message });
+    }
+};
